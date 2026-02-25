17ecb5000262fe60867fce909c5f6c2ba82310bd
nandini bhawre <nandinibhawre24@gmailcom>
2026-02-25 12:57:02 +0530

    update demo

--- src/main/java/com/Elite/Erum_Makeover/Controller/DemoRegistrationController.java
+++ src/main/java/com/Elite/Erum_Makeover/Controller/DemoRegistrationController.java
@@ -1,6 +1,7 @@
 package com.Elite.Erum_Makeover.Controller;
 
 import com.Elite.Erum_Makeover.Model.DemoRegistration;
+import com.Elite.Erum_Makeover.SecurityConfig.JwtUtil;
 import com.Elite.Erum_Makeover.Services.DemoRegistrationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,22 +17,29 @@ import java.util.List;
 public class DemoRegistrationController {
 
     private final DemoRegistrationService service;
-
-    // ✅ USER MUST LOGIN TO REGISTER
+    private  final JwtUtil jwtUtil;
     @PostMapping("/register")
     @PreAuthorize("hasAnyRole('USER')")
     public DemoRegistration register(
             @RequestBody DemoRegistration demo,
-            Authentication auth
+            Authentication auth,
+            @RequestHeader("Authorization") String authHeader
     ) {
-        // get logged in email from token
-        String email = auth.getName();
 
-        demo.setEmail(email); // override with logged-in email
+        // Remove "Bearer "
+        String token = authHeader.substring(7);
+
+        // Extract details from JWT
+        String email = jwtUtil.extractEmail(token);
+        String userId = jwtUtil.extractUserId(token);
+
+        // Set into entity
+        demo.setEmail(email);
+        demo.setUserId(userId);
+
         return service.register(demo);
     }
 
-    // ✅ ADMIN CAN VIEW ALL REGISTRATIONS
     @GetMapping("/all")
     @PreAuthorize("hasRole('ADMIN')")
     public List<DemoRegistration> all() {
