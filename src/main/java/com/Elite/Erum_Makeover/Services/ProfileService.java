5c2405f6b1ccf0a31bda254c9f3256dea7b494bc
nandini bhawre <nandinibhawre24@gmailcom>
2026-02-25 22:57:53 +0530

    add

--- src/main/java/com/Elite/Erum_Makeover/Services/ProfileService.java
+++ src/main/java/com/Elite/Erum_Makeover/Services/ProfileService.java
@@ -1,30 +1,17 @@
 package com.Elite.Erum_Makeover.Services;
-
 import com.Elite.Erum_Makeover.DTO.ProfileRequest;
 import com.Elite.Erum_Makeover.Model.Profile;
 import com.Elite.Erum_Makeover.Repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
-import java.time.LocalDate;
-import java.util.UUID;
-
 @Service
 @RequiredArgsConstructor
 public class ProfileService {
 
     private final ProfileRepository profileRepository;
-    private final S3Service s3Service;   // 👈 inject here
 
     public Profile createProfile(ProfileRequest request) {
-
-        String imageUrl = null;
-
-        if (request.getProfilePhoto() != null && !request.getProfilePhoto().isEmpty()) {
-            imageUrl = s3Service.uploadFile(request.getProfilePhoto(), "profiles");
-        }
 
         Profile profile = new Profile();
         profile.setFullName(request.getFullName());
@@ -49,8 +36,8 @@ public class ProfileService {
         profile.setCareerGoal(request.getCareerGoal());
         profile.setMessage(request.getMessage());
 
-        // ✅ Save S3 URL in DB
-        profile.setProfilePhoto(imageUrl);
+        // ✅ Just store URL received from frontend
+        profile.setProfilePhoto(request.getProfilePhoto());
 
         return profileRepository.save(profile);
     }
