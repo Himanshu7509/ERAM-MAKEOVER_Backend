325aa1f829009f89e794f8fed9ce2ab386c8dffc
nandini bhawre <nandinibhawre24@gmailcom>
2026-02-27 11:54:53 +0530

    updated controller

--- src/main/java/com/Elite/Erum_Makeover/Controller/ProfileController.java
+++ src/main/java/com/Elite/Erum_Makeover/Controller/ProfileController.java
@@ -1,10 +1,16 @@
 package com.Elite.Erum_Makeover.Controller;
 import com.Elite.Erum_Makeover.DTO.ProfileRequest;
+import com.Elite.Erum_Makeover.Model.Image;
 import com.Elite.Erum_Makeover.Model.Profile;
+import com.Elite.Erum_Makeover.Repository.ImageRepository;
+import com.Elite.Erum_Makeover.Repository.ProfileRepository;
 import com.Elite.Erum_Makeover.Services.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -14,16 +20,36 @@ import org.springframework.web.bind.annotation.*;
 public class ProfileController {
 
     private final ProfileService profileService;
-
+    private final ProfileRepository profileRepository;
+    private final ImageRepository imageRepository;
     // ✅ Create Profile (PURE JSON)
     @PostMapping
     public ResponseEntity<?> createProfile(@RequestBody ProfileRequest request) {
         return ResponseEntity.ok(profileService.createProfile(request));
     }
 
-    // ✅ Get Profile With Image
     @GetMapping("/{id}")
-    public ResponseEntity<?> getProfile(@PathVariable String id) {
-        return ResponseEntity.ok(profileService.getProfile(id));
+    public Map<String, Object> getProfile(@PathVariable String id) {
+
+        Profile profile = profileRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Profile not found"));
+
+        String imageUrl = null;
+
+        if (profile.getImageid() != null) {
+            Image image = imageRepository
+                    .findById(profile.getImageid())
+                    .orElse(null);
+
+            if (image != null) {
+                imageUrl = image.getImageUrl();
+            }
+        }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("profile", profile);
+        response.put("imageUrl", imageUrl);
+
+        return response;
     }
 }
