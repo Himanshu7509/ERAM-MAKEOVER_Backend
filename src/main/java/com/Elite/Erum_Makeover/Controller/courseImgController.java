bc4705fbc70744a0dacf0f8c41d55c32d0c1a83a
nandini bhawre <nandinibhawre24@gmailcom>
2026-03-02 11:25:59 +0530

    added course img

--- src/main/java/com/Elite/Erum_Makeover/Controller/courseImgController.java
+++ src/main/java/com/Elite/Erum_Makeover/Controller/courseImgController.java
@@ -1,5 +1,40 @@
 package com.Elite.Erum_Makeover.Controller;
 
+import com.Elite.Erum_Makeover.Model.Course;
+import com.Elite.Erum_Makeover.Model.Image;
+import com.Elite.Erum_Makeover.Repository.CourseRepository;
+import com.Elite.Erum_Makeover.Services.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
 public class courseImgController
+
 {
+    private final CourseRepository courseRepository;
+    private S3Service s3Service;
+    @PostMapping("/{courseId}/upload-image")
+    public ResponseEntity<?> uploadCourseImage(
+            @PathVariable String courseId,
+            @RequestParam("file") MultipartFile file
+    ) {
+
+        // 🔥 Find Course
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new RuntimeException("Course not found"));
+
+        Image image = s3Service.uploadFile(file, "ErumMakeover/Courses");
+
+        course.setImageUrl(image.getImageUrl());
+
+        courseRepository.save(course);
+
+        return ResponseEntity.ok(course);
+    }
 }
--- src/main/java/com/Elite/Erum_Makeover/Repository/CourseRepository.java
+++ src/main/java/com/Elite/Erum_Makeover/Repository/CourseRepository.java
@@ -3,4 +3,7 @@ package com.Elite.Erum_Makeover.Repository;
 import com.Elite.Erum_Makeover.Model.Course;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CourseRepository extends MongoRepository<Course,String> {}
+public interface CourseRepository extends MongoRepository<Course,String>
+{
+
+}
