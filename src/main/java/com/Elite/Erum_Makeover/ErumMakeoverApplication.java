d1efaefdd02d838cf0bfb6d4a91ef6181083541d
nandini bhawre <nandinibhawre24@gmailcom>
2026-04-05 22:02:15 +0530

    check

--- src/main/java/com/Elite/Erum_Makeover/ErumMakeoverApplication.java
+++ src/main/java/com/Elite/Erum_Makeover/ErumMakeoverApplication.java
@@ -1,5 +1,7 @@
 package com.Elite.Erum_Makeover;
 
+
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,6 +12,13 @@ public class ErumMakeoverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ErumMakeoverApplication.class, args);
-	}
 
+	}
+	@PostConstruct
+	public void checkRedisEnv() {
+		System.out.println("HOST: " + System.getenv("REDISHOST"));
+		System.out.println("PORT: " + System.getenv("REDISPORT"));
+	}
 }
+
+
