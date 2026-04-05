16150d80e215cfce6c1fe30a632cd4fcfb48c7b7
nandini bhawre <nandinibhawre24@gmailcom>
2026-04-06 00:10:43 +0530

    a

--- src/main/java/com/Elite/Erum_Makeover/Config/RedisConfig.java
+++ src/main/java/com/Elite/Erum_Makeover/Config/RedisConfig.java
@@ -1,35 +1,35 @@
-package com.Elite.Erum_Makeover.Config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-
-@Configuration
-public class RedisConfig {
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-
-        String host = System.getenv("REDISHOST");
-        String portStr = System.getenv("REDISPORT");
-        String password = System.getenv("REDISPASSWORD");
-
-        System.out.println("HOST: " + host);
-        System.out.println("PORT: " + portStr);
-        System.out.println("PASS: " + password);
-        int port = Integer.parseInt(portStr);
-
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName(host);
-        config.setPort(port);
-        config.setPassword(password);
-
-        // 🔥 MOST IMPORTANT LINE (THIS FIXES YOUR ERROR)
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                .useSsl()   // ✅ MUST ENABLE
-                .build();
-
-        return new LettuceConnectionFactory(config, clientConfig);
-    }
-}
+//package com.Elite.Erum_Makeover.Config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+//import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+//
+//@Configuration
+//public class RedisConfig {
+//    @Bean
+//    public LettuceConnectionFactory redisConnectionFactory() {
+//
+//        String host = System.getenv("REDISHOST");
+//        String portStr = System.getenv("REDISPORT");
+//        String password = System.getenv("REDISPASSWORD");
+//
+//        System.out.println("HOST: " + host);
+//        System.out.println("PORT: " + portStr);
+//        System.out.println("PASS: " + password);
+//        int port = Integer.parseInt(portStr);
+//
+//        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+//        config.setHostName(host);
+//        config.setPort(port);
+//        config.setPassword(password);
+//
+//        // 🔥 MOST IMPORTANT LINE (THIS FIXES YOUR ERROR)
+//        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+//                .useSsl()   // ✅ MUST ENABLE
+//                .build();
+//
+//        return new LettuceConnectionFactory(config, clientConfig);
+//    }
+//}
